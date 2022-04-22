de63affd883e2ff56db50eb5c05fa506480c1c5e
PaolaAndreaGA <paga0209@gmial.com>
2022-04-21 19:07:17 -0500

    fix pep8 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,17 +3,11 @@
 
 def canUnlockAll(boxes):
     """Unlock array of boxes of keys with indices"""
-    size = len(boxes)
-    checker = {}
-    index = 0
-
-    for keys in boxes:
-        if len(keys) == 0 or index == 0:
-            checker[index] = -1  # -1 means box is empty
-        for key in keys:
-            if key < size and key != index:
-                checker[key] = key
-        if len(checker) == size:
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
+        if len(myKeys) == len(boxes):
             return True
-        index += 1
-    return False
+        return False
