7504c999ef33c8fc99856311a732a7581753f8bc
PaolaAndreaGA <paga0209@gmial.com>
2022-05-05 17:48:54 -0500

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,48 @@
 #!/usr/bin/python3
-""" reads stdin line by line and computes metrics """
+
+'''
+Reads stdin line by line and computes metrics
+'''
 import sys
 
-
 if __name__ == "__main__":
-    status = {"200": 0,
-              "301": 0,
-              "400": 0,
-              "401": 0,
-              "403": 0,
-              "404": 0,
-              "405": 0,
-              "500": 0}
-    count = 1
-    file_size = 0
 
-    def get_line(line):
-        """ parse and grab data"""
-        try:
-            parsed_line = line.split()
-            status_code = parsed_line[-2]
-            if status_code in status.keys():
-                status[status_code] += 1
-            return int(parsed_line[-1])
-        except Exception:
-            return 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
 
     def print_stats():
-        """print stats"""
-        print("File size: {}".format(file_size))
-        for key in sorted(status.keys()):
-            if status[key]:
-                print("{}: {}".format(key, status[key]))
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
     try:
         for line in sys.stdin:
-            file_size += get_line(line)
+            parse_stdin(line)
+            # print the stats after every 10 outputs
             if count % 10 == 0:
                 print_stats()
             count += 1
